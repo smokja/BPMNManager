7955b3b1d0de72a34e04e75726005b16c535f263
Igor Cetkovic <cetkovic@gmx.ch>
2019-01-24 22:46:41 +0100

    create some basic code to start with

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+            
+        this.state = {
+            tasks: []    
+        };
+        
+        // check the api and redo this
+        fetch('camunda/api/tasks')
+            .then(x => this.setState({tasks: x}));
+    }
+    
+    render() {
+        return <div>
+            
+            <h1>Task list</h1>
+            
+            
+            
+        </div>
+    }
 }
-
-export default App;
