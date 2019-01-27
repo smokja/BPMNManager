f7f234674988971f603c7f4ee9d3f6777aa33041
Smokja <igor.cetkovic@edu.tbz.ch>
2019-01-27 17:26:26 +0100

    back to old port

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
         };
         
         // check the api and redo this
-        fetch('http://localhost:8080/engine-rest/task')
+        fetch('http://localhost/camunda/engine-rest/task')
         .then(res => res.json())
         .then(json => this.setState({tasks: json}))
         .catch();
